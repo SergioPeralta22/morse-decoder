6bb696493cdeed1df23dab001ca215b31e7ca8ec
Sergio Peralta <seperalta88@gmail.com>
2023-07-18 12:17:03 -0300

    add rubocop error

--- morse_decoder.rb
+++ morse_decoder.rb
@@ -1,39 +1,30 @@
-def morse_decoder(morse) #retuns a char or space if morse code is not found
-
+# retuns a char or space if morse code is not found
+def morse_decoder(morse)
   # morse code to char
   morse_to_char = {
-    ".-" => "A",
-    "-..." => "B",
-    "-.-." => "C",
-    "-.." => "D",
-    "." => "E",
-    "..-." => "F",
-    "--." => "G",
-    "...." => "H",
-    ".." => "I",
-    ".---" => "J",
-    "-.-" => "K",
-    ".-.." => "L",
-    "--" => "M",
-    "-." => "N",
-    "---" => "O",
-    ".--." => "P",
-    "--.-" => "Q",
-    ".-." => "R",
-    "..." => "S",
-    "-" => "T",
-    "..-" => "U",
-    "...-" => "V",
-    ".--" => "W",
-    "-..-" => "X",
-    "-.--" => "Y",
-    "--.." => "Z"
+    '.-' => 'A', '-...' => 'B', '-.-.' => 'C', '-..' => 'D', '.' => 'E', '..-.' => 'F', '--.' => 'G', '....' => 'H',
+    '..' => 'I', '.---' => 'J', '-.-' => 'K',
+    '.-..' => 'L',
+    '--' => 'M',
+    '-.' => 'N',
+    '---' => 'O',
+    '.--.' => 'P',
+    '--.-' => 'Q',
+    '.-.' => 'R',
+    '...' => 'S',
+    '-' => 'T',
+    '..-' => 'U',
+    '...-' => 'V',
+    '.--' => 'W',
+    '-..-' => 'X',
+    '-.--' => 'Y',
+    '--..' => 'Z'
   }
 
-  morse_to_char[morse] || " "
+  morse_to_char[morse] || ' '
 end
 
-def decode_word(word) # decode the word witout spaces
-  word.split(" ").map { |morse| morse_decoder(morse) }.join
+# decode the word witout spaces
+def decode_word(word)
+  word.split.map { |morse| morse_decoder(morse) }.join
 end
-
